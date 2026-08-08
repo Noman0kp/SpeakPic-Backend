3214d2e50fef4ccf4f36b513ab6a393eb9e21630
Noman0kp <nomankp1107@gmail.com>
2026-08-08 17:27:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/speakpic/SpeakpicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.speakpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakpicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakpicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speakpic/SpeakpicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakpic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakpicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
